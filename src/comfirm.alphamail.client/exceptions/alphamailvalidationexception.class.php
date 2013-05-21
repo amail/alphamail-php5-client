31140971a34cebf5f6a1c6556541323ec3a0e6f2
Robin Orheden <robin@zapzapgo.com>
2013-05-22 00:12:13 +0200

    Updated inconsistent license information

--- src/comfirm.alphamail.client/exceptions/alphamailvalidationexception.class.php
+++ src/comfirm.alphamail.client/exceptions/alphamailvalidationexception.class.php
@@ -3,8 +3,8 @@
     /*
     The MIT License
 
-    Copyright (c) 2011 Comfirm <http://www.comfirm.se/>
-
+    Copyright (c) Robin Orheden, 2013 <http://amail.io/>
+    
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
     in the Software without restriction, including without limitation the rights
